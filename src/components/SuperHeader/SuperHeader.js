5ec7f6e9f670f6c2e6ba625281e942a7bffe9931
Markus Lasermann <github@ml74.de>
2022-12-18 14:53:42 +0100

    Exercise 01 Superheader

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,35 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import SearchInput from '../SearchInput';
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-
-const SuperHeader = () => {
-  return (
-    <Wrapper>
-      <MarketingMessage>
-        Free shipping on domestic orders over $75!
-      </MarketingMessage>
-      <SearchInput />
-      <HelpLink href="/help">Help</HelpLink>
-      <UnstyledButton>
-        <Icon id="shopping-bag" strokeWidth={1} />
-      </UnstyledButton>
-    </Wrapper>
-  );
-};
+import SearchInput from "../SearchInput";
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
 
 const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+  display: flex;
+  justify-content: flex-start;
+  gap: 1rem;
+  padding: 0.5rem 1rem;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+  flex-grow: 1;
 `;
 
 const HelpLink = styled.a`
@@ -41,5 +31,18 @@ const HelpLink = styled.a`
     outline: none;
   }
 `;
+
+const SuperHeader = () => (
+  <Wrapper>
+    <MarketingMessage>
+      Free shipping on domestic orders over $75!
+    </MarketingMessage>
+    <SearchInput />
+    <HelpLink href="/help">Help</HelpLink>
+    <UnstyledButton>
+      <Icon id="shopping-bag" strokeWidth={1} />
+    </UnstyledButton>
+  </Wrapper>
+);
 
 export default SuperHeader;
